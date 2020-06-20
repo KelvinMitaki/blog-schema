b3ecde7c162385d6ef5f1188ca60ad3a9b762fb8
kelvinmitaki <mitakikelvin1@gmail.com>
2020-06-20 20:10:34 +0300

    added user and blog schema

---
+++ models/Blog.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    user: { type: mongoose.Types.ObjectId, ref: "User", required: true }
+  },
+  { timestamps: true }
+);
